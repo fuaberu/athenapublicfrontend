6700489418492994d5cfee6adc3b5220ba7d1dfd
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-07-03 01:56:40 -0300

    Fix layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { onNavigate } from '$app/navigation';
 	import { fade } from 'svelte/transition';
 	import { browser } from '$app/environment';
+	import ThemeSwitch from '$lib/ui/theme-switch.svelte';
 
 	const hasViewTransition = !!(browser && (document as any)).startViewTransition;
 
@@ -24,10 +25,25 @@
 	<title>Athena Public</title>
 </svelte:head>
 
-<main class="relative min-h-screen text-gray-950 dark:text-gray-50">
-	{#key data.pathname}
-		<div in:fade={{ duration: !hasViewTransition ? 300 : 0 }}>
-			<slot />
-		</div>
-	{/key}
+<main class="relative flex min-h-screen flex-col text-gray-950 dark:text-gray-50">
+	<header class="flex justify-between px-3 pt-3">
+		<a href="/">Home</a>
+		<ThemeSwitch isDark={data.theme === 'dark'} />
+	</header>
+	<div class="relative flex flex-1 flex-col">
+		{#key data.pathname}
+			<div
+				class="flex flex-1 flex-col items-center justify-center gap-3"
+				in:fade={{ duration: !hasViewTransition ? 300 : 0 }}
+			>
+				<slot />
+			</div>
+		{/key}
+	</div>
+	<footer class="mt-auto flex w-full justify-between bg-sky-700 p-6 dark:bg-sky-950">
+		<span>
+			Copyright © {new Date().getFullYear()}
+		</span>
+		<a href="https://www.portaldalogon.com.br" class="hover:underline">Logon Informática</a>
+	</footer>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="space-y-16 pt-16 text-center">
+<div class="space-y-16 text-center">
 	<h1 class="text-6xl font-bold">Athena Public</h1>
 	<p>O novo sistema da Logon</p>
 	<a class="btn btn-primary" href="/login">Login</a>
